9ed6278cd9d6ce024809f05225d2a52da9730af1
anonymous <64162091+anonymous770@users.noreply.github.com>
2020-12-17 20:02:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import os
 
-os.system('cp -r pushc /bin/')
-os.system('cp -r push /bin/')
-os.system('cp -r pus /bin/')
-os.system('cp -r status /bin/')
+os.system('cp -r pushc /usr/bin/')
+os.system('cp -r push /usr/bin/')
+os.system('cp -r pus /usr/bin/')
+os.system('cp -r status /usr/bin/')
 os.system('cp -r .gitignore ..')
 os.system('cp -r .commit_push.py ..')
 os.system('cp -r .push.py ..')
-os.chdir('/bin')
+os.chdir('/usr/bin')
 os.system('chmod +x push')
 os.system('chmod +x pushc')
 os.system('chmod +x pus')
